ab3d71ba5c24ae10121c5f3aa8e717b6f2c06681
Dan <hello@danrowden.com>
2023-12-06 20:31:24 +0200

    Added types for data variables and properties

--- src/index.ts
+++ src/index.ts
@@ -56,11 +56,16 @@ interface TransactionalNestedError {
   transactionalId?: string;
 }
 
-type TransactionalResponse = TransactionalSuccess | TransactionalError | TransactionalNestedError;
+type TransactionalResponse =
+  | TransactionalSuccess
+  | TransactionalError
+  | TransactionalNestedError;
 
+type ContactProperties = Record<string, string | number | boolean>;
+
+type TransactionalVariables = Record<string, string | number>;
 
 export default class LoopsClient {
-
   apiKey: string;
   apiRoot = "https://app.loops.so/api/";
 
@@ -70,14 +75,19 @@ export default class LoopsClient {
 
   /**
    * Creates and sends a query to the Loops API.
-   * 
+   *
    * @param {Object} params
    * @param {string} params.path Endpoint path
    * @param {string} params.method HTTP method
    * @param {Object} params.payload Payload for PUT and POST requests
    * @param {Object} params.params URL query parameters
    */
-  private async _makeQuery({ path, method = "GET", payload, params }: QueryOptions) {
+  private async _makeQuery({
+    path,
+    method = "GET",
+    payload,
+    params,
+  }: QueryOptions) {
     const headers = new Headers();
     headers.set("Authorization", `Bearer ${this.apiKey}`);
 
@@ -92,7 +102,7 @@ export default class LoopsClient {
       const response = await fetch(url.href, {
         method,
         headers,
-        body: payload ? JSON.stringify(payload) : undefined
+        body: payload ? JSON.stringify(payload) : undefined,
       });
       return await response.json();
     } catch (error) {
@@ -102,138 +112,152 @@ export default class LoopsClient {
 
   /**
    * Create a new contact.
-   * 
+   *
    * @param {string} email The email address of the contact.
    * @param {Object} [properties] All other contact properties, including custom properties.
-   * 
+   *
    * @see https://loops.so/docs/add-users/api-reference#add
-   * 
+   *
    * @returns {Object} Contact record or error response (JSON)
    */
-  async createContact(email: string, properties?: Record<string, any>): Promise<ContactSuccessResponse | ErrorResponse> {
+  async createContact(
+    email: string,
+    properties?: ContactProperties
+  ): Promise<ContactSuccessResponse | ErrorResponse> {
     const payload = { email, ...properties };
     return this._makeQuery({
       path: "v1/contacts/create",
       method: "POST",
-      payload
+      payload,
     });
   }
 
   /**
    * Update a contact.
-   * 
+   *
    * @param {string} email The email address of the contact.
    * @param {Object} properties All other contact properties, including custom properties.
-   * 
+   *
    * @see https://loops.so/docs/add-users/api-reference#update
-   * 
+   *
    * @returns {Object} Contact record or error response (JSON)
    */
-  async updateContact(email: string, properties: Record<string, any>): Promise<ContactSuccessResponse | ErrorResponse> {
+  async updateContact(
+    email: string,
+    properties: ContactProperties
+  ): Promise<ContactSuccessResponse | ErrorResponse> {
     const payload = { email, ...properties };
     return this._makeQuery({
       path: "v1/contacts/update",
       method: "PUT",
-      payload
+      payload,
     });
   }
 
   /**
    * Find a contact by email address.
-   * 
+   *
    * @param {string} email The email address of the contact.
-   * 
+   *
    * @see https://loops.so/docs/add-users/api-reference#find
-   * 
+   *
    * @returns {Object} List of contact records (JSON)
    */
   async findContact(email: string): Promise<Contact[]> {
     return this._makeQuery({
       path: "v1/contacts/find",
-      params: { email }
+      params: { email },
     });
   }
 
   /**
    * Delete a contact by email or user ID.
-   * 
+   *
    * @param {Object} params
    * @param {string} [params.email] The email address of the contact.
    * @param {string} [params.userId] The user ID of the contact.
-   * 
+   *
    * @see https://loops.so/docs/add-users/api-reference#delete
-   * 
+   *
    * @returns {Object} Confirmation or error response (JSON)
    */
-  async deleteContact(
-    { email, userId }: { email?: string, userId?: string }
-  ): Promise<DeleteSuccessResponse | ErrorResponse> {
-    const payload: { email?: string, userId?: string } = {}
+  async deleteContact({
+    email,
+    userId,
+  }: {
+    email?: string;
+    userId?: string;
+  }): Promise<DeleteSuccessResponse | ErrorResponse> {
+    const payload: { email?: string; userId?: string } = {};
     if (email) payload["email"] = email;
     else if (userId) payload["userId"] = userId;
     else throw "You must provide an `email` or `userId` value.";
     return this._makeQuery({
       path: "v1/contacts/delete",
       method: "POST",
-      payload
+      payload,
     });
   }
 
   /**
    * Send an event.
-   * 
+   *
    * @param {string} email The email address of the contact.
    * @param {string} eventName The name of the event.
    * @param {Object} [properties] Properties to update the contact with, including custom properties.
-   * 
+   *
    * @see https://loops.so/docs/add-users/api-reference#send
-   * 
+   *
    * @returns {Object} Response (JSON)
    */
-  async sendEvent(email: string, eventName: string, properties?: Record<string, any>): Promise<EventResponse> {
+  async sendEvent(
+    email: string,
+    eventName: string,
+    properties?: ContactProperties
+  ): Promise<EventResponse> {
     const payload = { email, eventName, ...properties };
     return this._makeQuery({
       path: "v1/events/send",
       method: "POST",
-      payload
+      payload,
     });
   }
 
   /**
    * Send a transactional email.
-   * 
+   *
    * @param {string} transactionalId The ID of the transactional email to send.
    * @param {string} email The email address of the recipient.
    * @param {Object} [dataVariables] Data variables as defined by the transational email template.
-   * 
+   *
    * @returns {Object} Confirmation or error response (JSON)
    */
   async sendTransactionalEmail(
     transactionalId: string,
     email: string,
-    dataVariables?: Record<string, string | number>
+    dataVariables?: TransactionalVariables
   ): Promise<TransactionalResponse> {
     const payload: {
-      transactionalId: string,
-      email: string,
-      dataVariables?: Record<string, string | number>
+      transactionalId: string;
+      email: string;
+      dataVariables?: TransactionalVariables;
     } = { transactionalId, email };
     if (dataVariables) payload["dataVariables"] = dataVariables;
     return this._makeQuery({
       path: "v1/transactional",
       method: "POST",
-      payload
+      payload,
     });
   }
 
   /**
    * Get custom fields/properties.
-   * 
+   *
    * @returns List of custom fields (JSON)
    */
   async getCustomFields(): Promise<Record<string, string>[]> {
     return this._makeQuery({
-      path: "v1/contacts/customFields"
+      path: "v1/contacts/customFields",
     });
   }
 }
