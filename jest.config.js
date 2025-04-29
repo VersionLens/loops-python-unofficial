988745d32d4da05d225bed485c1ac3c2a7d90e5d
Dan <dan@loops.so>
2025-04-29 11:12:24 +0300

    Tests

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+}; 
